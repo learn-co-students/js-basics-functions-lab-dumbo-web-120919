6bf7226e42ea8abc3d594a3550a3121622e861a4
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-01-26 14:16:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function distanceFromHqInBlocks(street) {
-  const distance;
+  let distance, absDistance;
+
   distance = street - 42;
-  Math.abs(distance);
-  return distance
+  absDistance = Math.abs(distance);
+  return absDistance;
 }
 
-function distanceFromHqInFeet() {
+function distanceFromHqInFeet(street) {
+  let blocksFromHq, distanceInFeet;
 
+  blocksFromHq = distanceFromHqInBlocks(street);
+  distanceInFeet = blocksFromHq * 264;
+  return distanceInFeet;
 }
 
-function distanceTravelledInFeet() {
+function distanceTravelledInFeet(start, end) {
+  let distance, absDistance, distanceInFeet;
 
+  distance = end - start;
+  absDistance = Math.abs(distance);
+  distanceInFeet = absDistance * 264;
+  return distanceInFeet;
 }
 
-function calculatesFarePrice(start, destination) {
-
+function calculatesFarePrice(start, end) {
+  let distance, fare;
+  
+  distance = distanceTravelledInFeet(start, end);
+  
+  if (distance < 400) {
+    fare = 0;
+  } else if (distance >= 400 && distance <= 2000) {
+    fare = 0.02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    fare = 25;
+  } else if (distance > 2500) {
+    return "cannot travel that far";
+  }
+  return fare 
 }
